03de33d2b01ab83c23694bd1100962caef9a1b56
guanshiming <guanshiming.love@163.com>
2015-09-19 20:24:06 +0800

    debug project

---
+++ build/classes/artifacts/black-hole/exploded/black-hole-1.0-SNAPSHOT.war/WEB-INF/views/index.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="/css/common.css" />
+    <title>Welcome to The Black-hole</title>
+</head>
+<body>
+  <div class="container">
+
+  </div>
+</body>
+</html>
---
+++ src/main/webapp/WEB-INF/js/templates/footer.hbs.js
@@ -0,0 +1,6 @@
+(function() {
+var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['footer'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<div class=\"page-footer\">\n    <div class=\"greyleft\">\n        <span class=\"btn-gray\"><a href=\"/company/changjiang\" class=\"color1 text_title\">长江汽车</a></span>\n    </div>\n    <div class=\"footer-menu\">\n        <div class=\"font1\">\n            <span><a class=\"mini color3 text_law_infor\" href=\"/company/law\">法律信息</a></span>\n            <span class=\"bar\">|</span>\n            <span><a class=\"mini color3 text_contect_me\" href=\"/company/about#contect_me\">联系我们</a></span>\n            <span class=\"bar\">|</span>\n            <span>\n                <img src=\"/WEB-INF/img/common/flag.png\" class=\"flag\" style=\"height: 15px; margin-top: 3px;\">\n                <a class=\"mini color3 text_china\">  中国</a>\n            </span>\n        </div>\n    </div>\n</div>";
+},"useData":true});
+}());
